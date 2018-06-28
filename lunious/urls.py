478f313309cc74b0bac1b65d9b426bf834548bb0
lunious <1164503275@qq.com>
2018-06-28 17:02:01 +0800

    2018/6/28 17:01

--- lunious/urls.py
+++ lunious/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from lunious import views
 
 urlpatterns = [
-    path('', views.index_1),
+    path('', views.index),
+    path('index_1/', views.index_1),
+    path('index_2/', views.index_2),
 ]
--- lunious/views.py
+++ lunious/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+# 首页_1
+def index(request):
+    return render(request, 'lunious/index.html')
 # 首页_1
 def index_1(request):
     return render(request, 'lunious/index_1.html')
+
+# 首页_2
+def index_2(request):
+    return render(request, 'lunious/index_2.html')
+
