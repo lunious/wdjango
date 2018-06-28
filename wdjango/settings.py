3c134ce5322bca2169b5948af7bad98595532055
lunious <1164503275@qq.com>
2018-06-28 09:17:19 +0800

    2018/6/28 9:17

--- wdjango/settings.py
+++ wdjango/settings.py
@@ -126,13 +126,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 # 开发阶段上传文件目录
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # 部署阶段上传文件目录
 # MEDIA_ROOT = '/var/www/wdjango/static'
+# STATIC_ROOT = '/var/www/wdjango/static'
 
 TINYMCE_DEFAULT_CONFIG = {
     'theme': 'advanced',
